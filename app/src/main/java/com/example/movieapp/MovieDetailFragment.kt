b65d0801b3b3d9d1fb4a8cc7db2e4cffa2c5e04f
Yordi Montero <monterocalvoyordi@gmail.com>
2021-04-22 15:41:08 -0600

    Primer Commit.

---
+++ app/src/main/java/com/example/movieapp/MovieDetailFragment.kt
@@ -0,0 +1,13 @@
+package com.example.movieapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.View
+
+class MovieDetailFragment : Fragment(R.layout.fragment_movie_detail) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
---
+++ app/src/main/java/com/example/movieapp/MovieFragment.kt
@@ -0,0 +1,13 @@
+package com.example.movieapp
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+
+class MovieFragment : Fragment(R.layout.fragment_movie) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
